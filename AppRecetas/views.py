6e3e190d356265460d16b21c189dd11ccb708eaa
bercun <bercun@gmail.com>
2023-09-19 22:53:49 -0300

    version 0.2 BD y receta pizza, model, views

--- AppRecetas/views.py
+++ AppRecetas/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from AppRecetas.models import *
+
 
 # Create your views here.
+
+def recetasMain(request):
+
+    receta_1 = RecetasMain(nom_platos= "pizza",
+     ingredientes= "harina, agua, levadura, sal, aceite , salsa de tomate",
+      receta= "harina 1kg, agua 0.5 litros, sal 10gr, levadura 10gr, sal 35, aceite 10cc",
+      tiempo= 30, 
+      dificultad= "media",
+      tipoDeCocina= "una hora",
+      fuente= "bercun",
+      procedimiento=" mezclar el harina con el agua, agregar levadura, amasar, agregar la sal, agregar el aceite, dejar leudar por media hora(en zona con humedad y calor suave) colocar en bandeja de horo, agregar la salsa de tomante y cocinar en horno medio"
+      )
+    receta_1.save()
+
+    return HttpResponse(f" La receta de {receta_1.nom_platos} \n es de dificultad {receta_1.dificultad} \n y los pasos de su elavoracion son {receta_1.procedimiento} ")  
+
+
+
--- recetasWeb/urls.py
+++ recetasWeb/urls.py
@@ -16,7 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from AppRecetas.views import *
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("recetasWeb/", recetasMain ),
+
+
 ]
